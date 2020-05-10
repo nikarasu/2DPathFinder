dedc0279075ef6a076035b0799ee02f8ab07681c
nikarasu <niklas.utterback@gmail.com>
2020-05-10 23:07:09 +0200

    Added = and == operator to Nick::Vector2

--- 2DPathFinder/structs.h
+++ 2DPathFinder/structs.h
@@ -4,10 +4,22 @@ namespace Nick
 {
 	struct Vector2
 	{
+		inline Vector2& operator=(const Vector2& aVec)
+		{
+			this->xPos = aVec.xPos;
+			this->yPos = aVec.yPos;
+			return *this;
+		}
+
 		int xPos;
 		int yPos;
 	};
 
+	const bool operator==(const Vector2& aVec0, const Vector2& aVec1)
+	{
+		return(aVec0.xPos == aVec1.xPos && aVec0.yPos == aVec1.yPos);
+	}
+	
 	struct Node
 	{
 		//Constructor and destructor can be expensive SEE CLASS
